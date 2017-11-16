4146c3c19d1608d1137f5b8d18b564528225df92
jheidema <jheid1205@gmail.com>
2017-11-16 16:34:17 -0500

    Forgot a couple of files for new geometry commit

---
+++ SimEnv.sh
@@ -0,0 +1,13 @@
+source /opt/root/5.34.36/bin/thisroot.sh
+source /opt/geant4/10.1.3/bin/geant4.sh
+
+export GENREFLEX_EXECUTABLE=/opt/root/5.34.36/bin/genreflex
+export Geant4_DIR=/opt/geant4/10.1.3/lib64/Geant4-10.1.3/
+export ROOTCINT_EXECUTABLE=/opt/root/5.34.36/bin/rootcint
+export ROOTSYS=/opt/root/5.34.36/
+export ROOT_CONFIG_EXECUTABLE=/opt/root/5.34.36/bin/root-config
+
+
+#export G4NEUTRONHPDATA=/opt/geant4/10.1.3/data/G4NDL4.5
+#export G4LEDATA=/opt/geant4/10.1.3/data/G4EMLOW6.41
+#export G4LEVELGAMMADATA=/opt/geant4/10.1.3/data/PhotonEvaporation3.1
--- neutronDetectorDesign.cc
+++ neutronDetectorDesign.cc
@@ -63,12 +63,12 @@ int main(int argc, char** argv)
 
 
   // Construct the default run manager
-#ifdef G4MULTITHREADED
-  G4MTRunManager* runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(G4Threading::G4GetNumberOfCores());
-#else
+//#ifdef G4MULTITHREADED
+//  G4MTRunManager* runManager = new G4MTRunManager;
+//  runManager->SetNumberOfThreads(G4Threading::G4GetNumberOfCores());
+//#else
   G4RunManager* runManager = new G4RunManager;
-#endif
+//#endif
 
 
   //G4RunManager *runManager = new G4RunManager;
