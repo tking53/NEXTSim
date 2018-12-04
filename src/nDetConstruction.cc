tking53/NEXTSim

commit 944c21ab05f26895e59d24e3bacdce4093a20749

Upgraded EJ200 photon emission spectrum
Added many more distribution points for EJ-200 photon
emission spectrum (increased from 5 to 33). Emission
distribution was digitized from Eljen spec sheet.

src/nDetConstruction.cc
@@ -939,38 +939,41 @@ void nDetConstruction::DefineMaterials() {
 	// EJ200 (C9H10)n
 	/////////////////////////////////////////////////////////////////
 
-    density = 1.023*g/cm3;
-
-    fEJ200 = new G4Material("EJ200", density, 2);
+    fEJ200 = new G4Material("EJ200", 1.023*g/cm3, 2);
     fEJ200->AddElement(fH, 10);
-    //no longer // removing carbon to see how that effects backward scatters... KS 5/23/16
-    fEJ200->AddElement(fC,9);
+    fEJ200->AddElement(fC, 9);
 
-    const G4int nEntries_EJ200 = 5;
-
-//   wavelength(nm) = 1242/energy(ev)
-//   so the corresponding wavelength in nm show next
-//   500, 475, 450, 425, 400
-
+    /*const G4int nEntries_EJ200 = 5;
     G4double PhotonEnergy[nEntries_EJ200] = { 2.484*eV, 2.615*eV, 2.760*eV, 2.922*eV, 3.105*eV };
     G4double RefractiveIndex_EJ200[nEntries_EJ200] = { 1.58, 1.58, 1.58, 1.58, 1.58 };
     G4double Absorption_EJ200[nEntries_EJ200] =  { 400*cm, 400*cm, 400*cm, 400*cm, 400*cm };
-    G4double ScintilFast_EJ200[nEntries_EJ200] = { 0.05, 0.25, 0.55, 1.00, 0.0};
+    G4double ScintilFast_EJ200[nEntries_EJ200] = { 0.05, 0.25, 0.55, 1.00, 0.0};*/
+
+    G4double PhotonEnergy[33] = {2.490*eV, 2.517*eV, 2.552*eV, 2.585*eV, 2.613*eV, 2.635*eV, 2.656*eV, 2.686*eV, 2.720*eV, 2.749*eV,
+                                 2.772*eV, 2.791*eV, 2.809*eV, 2.826*eV, 2.842*eV, 2.861*eV, 2.884*eV, 2.919*eV, 2.946*eV, 2.954*eV, 
+                                 2.961*eV, 2.967*eV, 2.974*eV, 2.981*eV, 2.987*eV, 2.994*eV, 3.001*eV, 3.009*eV, 3.018*eV, 3.029*eV, 
+                                 3.041*eV, 3.056*eV, 3.083*eV};
+
+    G4double ScintilFast_EJ200[33] = {0.060, 0.084, 0.122, 0.175, 0.234, 0.294, 0.356, 0.416, 0.473, 0.533,
+                                      0.594, 0.657, 0.720, 0.784, 0.846, 0.903, 0.962, 1.000, 0.917, 0.857, 
+                                      0.798, 0.732, 0.669, 0.604, 0.542, 0.480, 0.422, 0.359, 0.297, 0.237, 
+                                      0.170, 0.105, 0.028};
+
+	G4double PhotonEnergy2[2] = {0.060, 0.028};
+	G4double RefIndex_EJ200[2] = {1.58, 1.58};
+	G4double Absorption_EJ200[2] = {400*cm, 400*cm};
 
     fEJ200MPT = new G4MaterialPropertiesTable();
-    fEJ200MPT->AddProperty("RINDEX",       PhotonEnergy, RefractiveIndex_EJ200, nEntries_EJ200);
-    fEJ200MPT->AddProperty("ABSLENGTH",    PhotonEnergy, Absorption_EJ200,      nEntries_EJ200);
-    fEJ200MPT->AddProperty("FASTCOMPONENT",PhotonEnergy, ScintilFast_EJ200,     nEntries_EJ200);
+    fEJ200MPT->AddProperty("RINDEX", PhotonEnergy2, RefIndex_EJ200, 2);
+    fEJ200MPT->AddProperty("ABSLENGTH", PhotonEnergy2, Absorption_EJ200, 2);
+    fEJ200MPT->AddProperty("FASTCOMPONENT", PhotonEnergy, ScintilFast_EJ200, 33);
 
-    fEJ200MPT->AddConstProperty("SCINTILLATIONYIELD", 1/keV); //10,000 Photons per MeV
-    fEJ200MPT->AddConstProperty("RESOLUTIONSCALE",1.0); // Intrinsic resolution
+    fEJ200MPT->AddConstProperty("SCINTILLATIONYIELD", 0.64*17400/MeV); // 64% of Anthracene
+    fEJ200MPT->AddConstProperty("RESOLUTIONSCALE", 1.0); // Intrinsic resolution
 
     //fEJ200MPT->AddConstProperty("RISETIMECONSTANT", 0.9*ns); Geant4 10.1 TODO
     fEJ200MPT->AddConstProperty("FASTSCINTILLATIONRISETIME", 0.9*ns);
-    //fEJ200MPT->AddConstProperty("FASTSCINTILLATIONRISETIME", 0.5*ns); //TODO DPL changing rise time to 500 ps
-
     fEJ200MPT->AddConstProperty("FASTTIMECONSTANT", 2.1*ns);
-    //fEJ200MPT->AddConstProperty("FASTTIMECONSTANT", 0.5*ns); //TODO DPL Get Back to 2.1 ns
     fEJ200MPT->AddConstProperty("YIELDRATIO",1);// the strength of the fast component as a function of total scintillation yield
 
     G4double pEF = fLightYieldScale; 

commit cc9c1a882a07a0833ea7ae4f85b0f5745425f68b

Extrapolated EJ200 photon emission to zero
Added values to EJ200 photon emission array in
order to bring the distribution all the way down
to zero. This has fixed the "stuck" electron bug
which was introduced in commit 944c21a.

Removed the "rough" fix from nDetSteppingAction.

src/nDetConstruction.cc
@@ -272,24 +272,26 @@ void nDetConstruction::DefineMaterials() {
     G4double Absorption_EJ200[nEntries_EJ200] =  { 400*cm, 400*cm, 400*cm, 400*cm, 400*cm };
     G4double ScintilFast_EJ200[nEntries_EJ200] = { 0.05, 0.25, 0.55, 1.00, 0.0};*/
 
-    G4double PhotonEnergy[33] = {2.490*eV, 2.517*eV, 2.552*eV, 2.585*eV, 2.613*eV, 2.635*eV, 2.656*eV, 2.686*eV, 2.720*eV, 2.749*eV,
-                                 2.772*eV, 2.791*eV, 2.809*eV, 2.826*eV, 2.842*eV, 2.861*eV, 2.884*eV, 2.919*eV, 2.946*eV, 2.954*eV, 
-                                 2.961*eV, 2.967*eV, 2.974*eV, 2.981*eV, 2.987*eV, 2.994*eV, 3.001*eV, 3.009*eV, 3.018*eV, 3.029*eV, 
-                                 3.041*eV, 3.056*eV, 3.083*eV};
+	G4double PhotonEnergy[44] = {2.004*eV, 2.058*eV, 2.112*eV, 2.166*eV, 2.220*eV, 2.274*eV, 2.328*eV, 2.382*eV, 2.436*eV, 2.490*eV, 
+		                         2.517*eV, 2.552*eV, 2.585*eV, 2.613*eV, 2.635*eV, 2.656*eV, 2.686*eV, 2.720*eV, 2.749*eV, 2.772*eV, 
+		                         2.791*eV, 2.809*eV, 2.826*eV, 2.842*eV, 2.861*eV, 2.884*eV, 2.919*eV, 2.946*eV, 2.954*eV, 2.961*eV, 
+		                         2.967*eV, 2.974*eV, 2.981*eV, 2.987*eV, 2.994*eV, 3.001*eV, 3.009*eV, 3.018*eV, 3.029*eV, 3.041*eV, 
+		                         3.056*eV, 3.083*eV, 3.137*eV, 3.191*eV};
 
-    G4double ScintilFast_EJ200[33] = {0.060, 0.084, 0.122, 0.175, 0.234, 0.294, 0.356, 0.416, 0.473, 0.533,
-                                      0.594, 0.657, 0.720, 0.784, 0.846, 0.903, 0.962, 1.000, 0.917, 0.857, 
-                                      0.798, 0.732, 0.669, 0.604, 0.542, 0.480, 0.422, 0.359, 0.297, 0.237, 
-                                      0.170, 0.105, 0.028};
-
-	G4double PhotonEnergy2[2] = {0.060, 0.028};
-	G4double RefIndex_EJ200[2] = {1.58, 1.58};
+	G4double ScintilFast_EJ200[44] = {0.000, 0.001, 0.001, 0.002, 0.003, 0.006, 0.010, 0.018, 0.033, 0.060, 
+		                              0.084, 0.122, 0.175, 0.234, 0.294, 0.356, 0.416, 0.473, 0.533, 0.594, 
+		                              0.657, 0.720, 0.784, 0.846, 0.903, 0.962, 1.000, 0.917, 0.857, 0.798, 
+		                              0.732, 0.669, 0.604, 0.542, 0.480, 0.422, 0.359, 0.297, 0.237, 0.170, 
+		                              0.105, 0.028, 0.004, 0.000};
+                                  
+	G4double PhotonEnergy2[2] = {2.004*eV, 3.191*eV};
+	G4double RefIndex_EJ200[2] = {1.580, 1.580};
 	G4double Absorption_EJ200[2] = {400*cm, 400*cm};
 
     fEJ200MPT = new G4MaterialPropertiesTable();
     fEJ200MPT->AddProperty("RINDEX", PhotonEnergy2, RefIndex_EJ200, 2);
     fEJ200MPT->AddProperty("ABSLENGTH", PhotonEnergy2, Absorption_EJ200, 2);
-    fEJ200MPT->AddProperty("FASTCOMPONENT", PhotonEnergy, ScintilFast_EJ200, 33);
+    fEJ200MPT->AddProperty("FASTCOMPONENT", PhotonEnergy, ScintilFast_EJ200, 44);
 
     //fEJ200MPT->AddConstProperty("SCINTILLATIONYIELD", 0.64*17400/MeV); // 64% of Anthracene
     fEJ200MPT->AddConstProperty("SCINTILLATIONYIELD", 10000/MeV); // Scintillation efficiency as per Eljen specs

src/nDetSteppingAction.cc
@@ -60,9 +60,6 @@ void nDetSteppingAction::UserSteppingAction(const G4Step* aStep)
     else // Normal scatter
       runAction->scatterNeutron(aStep);
   }
-  else if(track->GetParticleDefinition()->GetParticleName() == "e-"){ // Kill all electrons immediately to avoid endless electron scattering.
-    track->SetTrackStatus(fStopAndKill);
-  }
   else if(track->GetTrackID() == 1){ // Enter the material.
     runAction->initializeNeutron(aStep);
     neutronTrack = true;