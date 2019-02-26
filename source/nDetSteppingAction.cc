tking53/NEXTSim

commit 278a54a80494e0bbea1dd0d9e91247b26819ee08

Updated scattering routines for shadow bars
Updated nDetStepping primary particle scattering
code to account for secondary scattering from
shadow bar.

source/nDetSteppingAction.cc
@@ -34,9 +34,9 @@
 
 nDetSteppingAction::nDetSteppingAction(nDetConstruction* det, nDetRunAction* runAct, nDetEventAction* evtAct) : detector(det), runAction(runAct), evtAction(evtAct)
 {
-  neutronTrack = false;
-  eventID = -1;
-  stepID = 0;
+	neutronTrack = false;
+	eventID = -1;
+	stepID = 0;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -48,36 +48,43 @@ nDetSteppingAction::~nDetSteppingAction()
 
 void nDetSteppingAction::UserSteppingAction(const G4Step* aStep)
 {
-  G4Track *track = aStep->GetTrack();
+	G4Track *track = aStep->GetTrack();
 
-  if(neutronTrack){
-    if(track->GetTrackID() != 1) 
-      neutronTrack = false;
-    else if(aStep->GetPreStepPoint()->GetMaterial()->GetName() == "G4_AIR"){ // Escape
-      runAction->finalizeNeutron(aStep);
-      neutronTrack = false;
-    }
-    else // Normal scatter
-      runAction->scatterNeutron(aStep);
-  }
-  else if(track->GetTrackID() == 1){ // Enter the material.
-    runAction->initializeNeutron(aStep);
-    neutronTrack = true;
-  }
-
-  // Simplifying this process to try and alleviate problems (CRT)
-  if(track->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()){
-    if (aStep->GetPostStepPoint()->GetStepStatus() == fGeomBoundary) {
-      G4String vName = aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName();
-      if(vName.find("psSiPM") != std::string::npos){
-        detector->AddDetectedPhoton(aStep);
-      }
-    }
-  }
+	if(aStep->GetPostStepPoint()->GetStepStatus() == fGeomBoundary){ // Check if the particle is at a geometry boundary.
+		G4String vName = aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName();
+		if(track->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()){
+			if(vName.find("psSiPM") != std::string::npos){
+				detector->AddDetectedPhoton(aStep);
+			}
+		}
+		else{
+			if(neutronTrack){
+				if(track->GetTrackID() != 1)
+					neutronTrack = false;
+				else if(vName == "expHall_physV"){ // Exiting the detector.
+					runAction->finalizeNeutron(aStep);
+					neutronTrack = false;
+				}
+				// Else do nothing (the particle is probably traversing the inner wrapping layers).
+			}
+			else if(track->GetTrackID() == 1 && aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName().find("Scint") != std::string::npos){ // Enter the material.
+				runAction->initializeNeutron(aStep);
+				neutronTrack = true;
+			}
+		}
+	}
+	else{ // Normal scattering event.
+		if(neutronTrack){
+			if(track->GetTrackID() != 1)
+				neutronTrack = false;
+			else
+				runAction->scatterNeutron(aStep);
+		}
+	}
 }
 
 void nDetSteppingAction::Reset(){
-  neutronTrack = false;
+	neutronTrack = false;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......

include/nDetRunAction.hh
@@ -43,7 +43,11 @@ class primaryTrackInfo{
     G4double kE, dkE;
     G4double gtime, plength;
     G4double angle;
-    
+
+	G4int copyNum;
+	G4int trackID;
+	G4int atomicMass;
+
     const G4ParticleDefinition *part;
     
     primaryTrackInfo(const G4Step *step);
@@ -59,6 +63,9 @@ class primaryTrackInfo{
 	double getPathLength(const G4ThreeVector &rhs);
 
     void print();
+    
+  private:
+	void setValues(const G4Track *track);
 };
 
 class nDetRunAction : public G4UserRunAction
@@ -190,7 +197,7 @@ class nDetRunAction : public G4UserRunAction
 
     bool toggleVerboseMode(){ return (verbose = !verbose); }
 
-    void scatterEvent(const G4Track *track);
+	void process();
 
     void initializeNeutron(const G4Step *step);
 
@@ -199,6 +206,8 @@ class nDetRunAction : public G4UserRunAction
     void finalizeNeutron(const G4Step *step);
 
   private:  // function member
+    bool scatterEvent();
+
     bool openRootFile(const G4Run* aRun);
     
     bool closeRootFile();

source/ParticleSource.cc
@@ -177,7 +177,6 @@ void ParticleSource::GeneratePrimaries(G4Event* anEvent)
 {
 	double energy = psource->sample();
 	particleGun->SetParticleEnergy(energy);
-	runAction->initEnergy = energy;
 	if(psource->getIsIsotropic()){ // Generate particles psuedo-isotropically
 		// We don't really use a true isotropic source because that would be really slow.
 		// Generate a random point inside the volume of the detector in the frame of the detector.

source/nDetConstruction.cc
@@ -623,11 +623,11 @@ void nDetConstruction::buildModule(){
 			// Place vertical and horizontal reflectors.
 			if(fMylarThickness > 0){ 
 				if(row == 0 && col != fNumColumns-1){ // New vertical reflector layer.
-					std::stringstream stream2; stream << "Wrapping-" << col;
+					std::stringstream stream2; stream2 << "Wrapping-" << col;
 					mylarVertLayer_physV[col] = new G4PVPlacement(0, G4ThreeVector(cellCenter.getX()+cellWidth/2+fMylarThickness/2, 0, 0), mylarVertLayer_logV, stream2.str().c_str(), assembly_logV, 0, 0, fCheckOverlaps);
 				}
 				if(row != fNumRows-1){ // New horizontal reflector layer.
-					std::stringstream stream2; stream << "Wrapping-" << col << "," << row;
+					std::stringstream stream2; stream2 << "Wrapping-" << col << "," << row;
 					mylarHorizLayer_physV[col][row] = new G4PVPlacement(0, G4ThreeVector(cellCenter.getX(), cellCenter.getY()+cellHeight/2+fMylarThickness/2, 0), mylarHorizLayer_logV, stream2.str().c_str(), assembly_logV, 0, 0, fCheckOverlaps);
 				}
 			}

source/nDetEventAction.cc
@@ -63,13 +63,16 @@ void nDetEventAction::BeginOfEventAction(const G4Event* evt){
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
  
 void nDetEventAction::EndOfEventAction(const G4Event*){
+  // Process primary scatters. 
+  runAct->process();
+
   // Fill branches
   runAct->fillBranch();
 
   // Update photon statistics.
   numPhotons += runAct->nPhotonsTot;
   numPhotonsDet += runAct->nPhotonsDet[0]+runAct->nPhotonsDet[1];
-        
+  
   // Clear vector for next event
   runAct->vectorClear();
 }

source/nDetRunAction.cc
@@ -33,24 +33,12 @@ double dotProduct(const G4ThreeVector &v1, const G4ThreeVector &v2){
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 primaryTrackInfo::primaryTrackInfo(const G4Step *step){
-	G4Track *track = step->GetTrack();
-	pos = track->GetPosition();
-	dir = track->GetMomentumDirection();
-	kE = track->GetKineticEnergy();
+	this->setValues(step->GetTrack());
 	dkE = -1*step->GetDeltaEnergy();
-	gtime = track->GetGlobalTime();
-	plength = track->GetTrackLength();
-	part = track->GetParticleDefinition();
 }
 
 primaryTrackInfo::primaryTrackInfo(const G4Track *track){
-	pos = track->GetPosition();
-	dir = track->GetMomentumDirection();
-	kE = track->GetKineticEnergy();
-	dkE = -1;
-	gtime = track->GetGlobalTime();
-	plength = track->GetTrackLength();
-	part = track->GetParticleDefinition();
+	this->setValues(track);
 }
 
 bool primaryTrackInfo::compare(const G4Track *rhs){
@@ -77,6 +65,20 @@ double primaryTrackInfo::getPathLength(const G4ThreeVector &rhs){
 
 void primaryTrackInfo::print(){
 	std::cout << "name=" << part->GetParticleName() << ", kE=" << kE << ", dkE=" << dkE << ", pos=(" << pos.getX() << ", " << pos.getY() << ", " << pos.getZ() << "), dir=(" << dir.getX() << ", " << dir.getY() << ", " << dir.getZ() << ")\n";
+}
+
+void primaryTrackInfo::setValues(const G4Track *track){
+	pos = track->GetPosition();
+	dir = track->GetMomentumDirection();
+	kE = track->GetKineticEnergy();
+	dkE = -1;
+	gtime = track->GetGlobalTime();
+	plength = track->GetTrackLength();
+	part = track->GetParticleDefinition();
+	
+	copyNum = track->GetTouchable()->GetCopyNumber();
+	trackID = track->GetTrackID();
+	atomicMass = part->GetAtomicMass();
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -436,6 +438,11 @@ bool nDetRunAction::fillBranch()
 	return true;
 }
 
+void nDetRunAction::process(){
+	while(this->scatterEvent()){
+	}
+}
+
 void nDetRunAction::vectorClear(){
 	nScatters = 0;
 	nPhotonsTot = 0;
@@ -499,22 +506,12 @@ void nDetRunAction::setActions(nDetEventAction *event_, nDetStackingAction *stac
 	counter = stacking->GetCounter();
 }
 
-void nDetRunAction::scatterEvent(const G4Track *track){
-	if(primaryTracks.empty()){
-		if(verbose) 
-			std::cout << " Warning: primaryTracks is empty!\n";
-		return;
-	}
+bool nDetRunAction::scatterEvent(){
+	if(primaryTracks.size() <= 1)
+		return false;
 
 	// Get a primary particle step off the stack.
 	primaryTrackInfo *priTrack = &primaryTracks.back();
-
-	if(!priTrack->compare(track)){
-		G4ThreeVector vec = track->GetPosition();
-		if(verbose) 
-			std::cout << " Warning: Scatter pos=(" << priTrack->pos.getX() << ", " << priTrack->pos.getY() << ", " << priTrack->pos.getZ() << ") does not match recoil pos=(" << vec.getX() << ", " << vec.getY() << ", " << vec.getZ() << "), primaryTracks.size()=" << primaryTracks.size() << std::endl;
-		return;
-	}
 
 	if(nScatters++ == 0){ // First scatter event (CAREFUL! These are in reverse order).
 		if(primaryTracks.size() < 2){
@@ -522,14 +519,14 @@ void nDetRunAction::scatterEvent(const G4Track *track){
 				std::cout << " Warning: Expected at least two events in primaryTracks, but primaryTracks.size()=" << primaryTracks.size() << std::endl;
 				priTrack->print();
 			}
-			return;
+			return false;
 		}
 		if(outputDebug){
 			G4ThreeVector firstScatter = primaryTracks.at(0).pos + primaryTracks.at(1).pos;
 			nTimeToFirstScatter = primaryTracks.at(1).gtime;
 			nLengthToFirstScatter = firstScatter.mag();
 		}
-		counter->push_back(track->GetTrackID());
+		counter->push_back(priTrack->trackID);
 	}
 	
 	// Add the deposited energy to the total.
@@ -538,10 +535,9 @@ void nDetRunAction::scatterEvent(const G4Track *track){
 	G4ThreeVector vertex = priTrack->pos;
 
 	if(verbose){
-		primaryTrackInfo pizza(track);
-		std::cout << " id=" << track->GetTrackID() << " "; pizza.print();	
+		std::cout << " id=" << priTrack->trackID << " "; priTrack->print();	
 		std::cout << "	pos=(" << vertex.getX() << ", " << vertex.getY() << ", " << vertex.getZ() << ")\n";
-		std::cout << "	dE=" << priTrack->dkE << ", dE2=" << track->GetKineticEnergy() << ", size=" << primaryTracks.size() << std::endl;
+		std::cout << "	dE=" << priTrack->dkE << ", dE2=" << priTrack->kE << ", size=" << primaryTracks.size() << std::endl;
 	}
 
 	if(outputDebug){
@@ -559,23 +555,26 @@ void nDetRunAction::scatterEvent(const G4Track *track){
 
 		nPathLength.push_back(priTrack->plength);
 		scatterTime.push_back(priTrack->gtime - incidentTime);
-		recoilMass.push_back(track->GetParticleDefinition()->GetAtomicMass()); 
+		recoilMass.push_back(priTrack->atomicMass); 
 
 		G4int segCol, segRow;
-		detector->GetSegmentFromCopyNum(track->GetTouchable()->GetCopyNumber(), segCol, segRow);
+		detector->GetSegmentFromCopyNum(priTrack->copyNum, segCol, segRow);
 		segmentCol.push_back(segCol);
 		segmentRow.push_back(segRow);
 	}
 	
 	primaryTracks.pop_back();
+	
+	return true;
 }
 
 void nDetRunAction::initializeNeutron(const G4Step *step){
+	initEnergy = step->GetPreStepPoint()->GetKineticEnergy();
 	if(outputDebug){
 		incidentTime = step->GetTrack()->GetGlobalTime();
-		neutronIncidentPositionX = step->GetPostStepPoint()->GetPosition().getX();
-		neutronIncidentPositionY = step->GetPostStepPoint()->GetPosition().getY();
-		neutronIncidentPositionZ = step->GetPostStepPoint()->GetPosition().getZ();
+		neutronIncidentPositionX = step->GetPreStepPoint()->GetPosition().getX();
+		neutronIncidentPositionY = step->GetPreStepPoint()->GetPosition().getY();
+		neutronIncidentPositionZ = step->GetPreStepPoint()->GetPosition().getZ();
 		timeInMaterial = 0;
 		neutronExitPositionX = 0;
 		neutronExitPositionY = 0;
@@ -618,9 +617,9 @@ void nDetRunAction::finalizeNeutron(const G4Step *step){
 	G4Track *track = step->GetTrack();
 	if(outputDebug){
 		timeInMaterial = track->GetGlobalTime() - incidentTime;
-		neutronExitPositionX = track->GetPosition().getX();
-		neutronExitPositionY = track->GetPosition().getY();
-		neutronExitPositionZ = track->GetPosition().getZ();
+		neutronExitPositionX = step->GetPreStepPoint()->GetPosition().getX();
+		neutronExitPositionY = step->GetPreStepPoint()->GetPosition().getY();
+		neutronExitPositionZ = step->GetPreStepPoint()->GetPosition().getZ();
 	}
 	if(verbose) 
 		std::cout << "OT: final kE=" << track->GetKineticEnergy() << std::endl;

source/nDetTrackingAction.cc
@@ -18,12 +18,4 @@ nDetTrackingAction::nDetTrackingAction(nDetRunAction *run) : runAction(run) {
 nDetTrackingAction::~nDetTrackingAction(){}
 
 void nDetTrackingAction::PreUserTrackingAction(const G4Track* aTrack){
-  if (aTrack->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()) { // Photon track
-  }
-  else if(aTrack->GetParentID() == 1){ // Scatter event
-    if(aTrack->GetTrackID() != prevTrackID){ // Skip cascades. We're only concerned with primary scattering
-      runAction->scatterEvent(aTrack);
-      prevTrackID = aTrack->GetTrackID();
-    }
-  }
 }

commit b0126e3d6a82fe27acde548536861f5c27cb358a

Fixed neutron scatter initialization error
Fixed a bug introduced in commit 278a54a where, in
some cases, the neutron scattering tracking would
not be started correctly. This error arose because
the program was only considering stepping events
where the neutron was at a geometry boundary AND
was within the scintillator material.

This error leads to situations where a significant
number of neutron scatter events are not tracked
correctly because their first scatter event is
within the first column of a segemented detector.
It also caused single-segment detectors to not work
at all because it would never track neutron scatters
in the scintillator body.

This error has been fixed, as far as I can tell.
I've tested both with multi and single segment
detectors and both now work as expected. This also
seems to fix the broken neutron enter positions
nEnterPosX, Y, and Z which have been broken for
some time.

source/nDetSteppingAction.cc
@@ -49,37 +49,25 @@ nDetSteppingAction::~nDetSteppingAction()
 void nDetSteppingAction::UserSteppingAction(const G4Step* aStep)
 {
 	G4Track *track = aStep->GetTrack();
-
-	if(aStep->GetPostStepPoint()->GetStepStatus() == fGeomBoundary){ // Check if the particle is at a geometry boundary.
-		G4String vName = aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName();
-		if(track->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()){
-			if(vName.find("psSiPM") != std::string::npos){
-				detector->AddDetectedPhoton(aStep);
-			}
-		}
-		else{
-			if(neutronTrack){
-				if(track->GetTrackID() != 1)
-					neutronTrack = false;
-				else if(vName == "expHall_physV"){ // Exiting the detector.
-					runAction->finalizeNeutron(aStep);
-					neutronTrack = false;
-				}
-				// Else do nothing (the particle is probably traversing the inner wrapping layers).
-			}
-			else if(track->GetTrackID() == 1 && aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName().find("Scint") != std::string::npos){ // Enter the material.
-				runAction->initializeNeutron(aStep);
-				neutronTrack = true;
-			}
-		}
+	if(track->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()){ // Check for detected optical photons.
+		if(aStep->GetPostStepPoint()->GetStepStatus() == fGeomBoundary && aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName().find("psSiPM") != std::string::npos)
+			detector->AddDetectedPhoton(aStep);
 	}
-	else{ // Normal scattering event.
-		if(neutronTrack){
-			if(track->GetTrackID() != 1)
-				neutronTrack = false;
-			else
-				runAction->scatterNeutron(aStep);
+	else if(track->GetTrackStatus() != fAlive) return;
+	else if(neutronTrack){ // Normal scattering event.
+		if(track->GetTrackID() != 1)
+			neutronTrack = false;
+		else if(aStep->GetPostStepPoint()->GetPhysicalVolume()->GetName() == "expHall_physV"){ // Exiting the detector.
+			runAction->finalizeNeutron(aStep);
+			neutronTrack = false;
+			return;
 		}
+		else
+			runAction->scatterNeutron(aStep);
+	}	
+	else if(track->GetTrackID() == 1 && aStep->GetPreStepPoint()->GetPhysicalVolume()->GetName().find("Scint") != std::string::npos){ // Enter the material.
+		runAction->initializeNeutron(aStep);
+		neutronTrack = true;
 	}
 }
 