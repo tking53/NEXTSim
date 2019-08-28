f45056559ec5bbe09f69fb25bd49b645946d05d7
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2019-08-28 19:33:22 -0400

    Modified terminal color strings

--- include/termColors.hh
+++ include/termColors.hh
@@ -4,55 +4,59 @@
 #include <string>
 
 namespace TermColors {
-  static const char DkRed[]     = "\033[0;31m";
-  static const char DkGreen[]   = "\033[0;32m";
-  static const char DkYellow[]  = "\033[0;33m";
-  static const char DkBlue[]    = "\033[0;34m";
-  static const char DkMagenta[] = "\033[0;35m";
-  static const char DkCyan[]    = "\033[0;36m";
-  static const char DkWhite[]   = "\033[0;37m";
+	static std::string DkRed     = "\033[0;31m";
+	static std::string DkGreen   = "\033[0;32m";
+	static std::string DkYellow  = "\033[0;33m";
+	static std::string DkBlue    = "\033[0;34m";
+	static std::string DkMagenta = "\033[0;35m";
+	static std::string DkCyan    = "\033[0;36m";
+	static std::string DkWhite   = "\033[0;37m";
 
-  static const char BtRed[]     = "\033[1;31m";
-  static const char BtGreen[]   = "\033[1;32m";
-  static const char BtYellow[]  = "\033[1;33m";
-  static const char BtBlue[]    = "\033[1;34m";
-  static const char BtMagenta[] = "\033[1;35m";
-  static const char BtCyan[]    = "\033[1;36m";
-  static const char BtWhite[]   = "\033[1;37m";
+	static std::string BtRed     = "\033[1;31m";
+	static std::string BtGreen   = "\033[1;32m";
+	static std::string BtYellow  = "\033[1;33m";
+	static std::string BtBlue    = "\033[1;34m";
+	static std::string BtMagenta = "\033[1;35m";
+	static std::string BtCyan    = "\033[1;36m";
+	static std::string BtWhite   = "\033[1;37m";
 
-  static const char Flashing[]  = "\033[5m";
-  static const char Underline[] = "\033[4m"; 
-  static const char Reset[]     = "\033[0m";
+	static std::string Flashing  = "\033[5m";
+	static std::string Underline = "\033[4m"; 
+	static std::string Reset     = "\033[0m";
 }
 
 namespace Display {
-  void SetColorTerm(void);
+	std::string CriticalStr(const std::string &name="");
 
-  std::string CriticalStr(const std::string &str);
-  
-  std::string ErrorStr(const std::string &str = "[ERROR]");
-  
-  std::string InfoStr(const std::string &str = "[INFO]");
-  
-  std::string OkayStr(const std::string &str = "[OK]");
-  
-  std::string WarningStr(const std::string &str = "[WARNING]");
+	std::string ErrorStr(const std::string &name="");
 
-  void CriticalPrint(const std::string &str, const std::string &name="");
-  
-  void ErrorPrint(const std::string &str, const std::string &name="");
-  
-  void InfoPrint(const std::string &str, const std::string &name="");
-  
-  void OkayPrint(const std::string &str, const std::string &name="");
-  
-  void WarningPrint(const std::string &str, const std::string &name="");
+	std::string InfoStr(const std::string &name="");
 
-  void LeaderPrint(const std::string &str);
-  
-  bool StatusPrint(bool errorStatus);
+	std::string OkayStr(const std::string &name="");
 
-  extern bool hasColorTerm; ///< Flag indicating that the terminal is capable of outputting colored text
+	std::string WarningStr(const std::string &name="");
+
+	std::string ResetStr();
+
+	void CriticalPrint(const std::string &str, const std::string &name="");
+
+	void ErrorPrint(const std::string &str, const std::string &name="");
+
+	void InfoPrint(const std::string &str, const std::string &name="");
+
+	void OkayPrint(const std::string &str, const std::string &name="");
+
+	void WarningPrint(const std::string &str, const std::string &name="");
+
+	void LeaderPrint(const std::string &str);
+
+	bool StatusPrint(bool errorStatus);
+
+	extern std::string fatalStr;
+	extern std::string errorStr;
+	extern std::string warnStr;
+	extern std::string infoStr;
+	extern std::string okayStr;
 }
 
 #endif
--- source/termColors.cc
+++ source/termColors.cc
@@ -2,25 +2,19 @@
 #include <iostream>
 
 #include <cstdlib>
+#include <sstream>
 
 #include "termColors.hh"
 
 using namespace TermColors;
 
-static const size_t leaderLength = 50;
+const size_t leaderLength = 50;
 
-bool Display::hasColorTerm = true;
-
-void Display::SetColorTerm(){
-#ifdef NOCOLORINTERFACE
-	hasColorTerm = false;
-#else
-	std::string termname = getenv("TERM");
-	
-	if (termname == "xterm")
-		hasColorTerm = true;
-#endif
-}
+std::string Display::fatalStr = BtRed + Flashing + "[FATAL ERROR] ";
+std::string Display::errorStr = DkRed + "[ERROR] ";
+std::string Display::warnStr = DkYellow + "[WARNING] ";
+std::string Display::infoStr = BtBlue + "[INFO] ";
+std::string Display::okayStr = DkGreen + "[OK] ";
 
 void Display::LeaderPrint(const std::string &str){
 	char ch = std::cout.fill();
@@ -30,75 +24,64 @@ void Display::LeaderPrint(const std::string &str){
 	std::cout.fill(ch);
 }
 
-std::string Display::CriticalStr(const std::string &str){
-	if (hasColorTerm)
-		return (std::string(BtRed) + Flashing + str + Reset);
-	return (std::string("###") + str + "###");
+std::string Display::CriticalStr(const std::string &name/*=""*/){
+	if(name.empty())
+		return fatalStr;
+	return (fatalStr + name + ": ");
 }
 
-std::string Display::ErrorStr(const std::string &str){
-	if (hasColorTerm)
-		return (DkRed + str + Reset);
-	return str;
+std::string Display::ErrorStr(const std::string &name/*=""*/){
+	if(name.empty())
+		return errorStr;
+	return (errorStr + name + ": ");
 }
 
-std::string Display::InfoStr(const std::string &str){
-	if (hasColorTerm)
-		return (BtBlue + str + Reset);
-	return str;
+std::string Display::InfoStr(const std::string &name/*=""*/){
+	if(name.empty())
+		return infoStr;
+	return (infoStr + name + ": ");
 }
 
-std::string Display::OkayStr(const std::string &str){
-	if (hasColorTerm)
-		return (DkGreen + str + Reset);
-	return str;
+std::string Display::OkayStr(const std::string &name/*=""*/){
+	if(name.empty())
+		return okayStr;
+	return (okayStr + name + ": ");
 }
 
-std::string Display::WarningStr(const std::string &str){
-	if (hasColorTerm)
-		return (DkYellow + str + Reset);
-	return str;
+std::string Display::WarningStr(const std::string &name/*=""*/){
+	if(name.empty())
+		return warnStr;
+	return (warnStr + name + ": ");
+}
+
+std::string Display::ResetStr(){
+	return Reset;
 }
 
 void Display::CriticalPrint(const std::string &str, const std::string &name/*=""*/){
-	std::cout << BtRed << Flashing << "[CRITICAL ERROR]";
-	if(!name.empty())
-		std::cout << " " << name << ":";
-	std::cout << " " << str << Reset << std::endl;
+	std::cout << CriticalStr(name) << str << Reset << std::endl;
 }
 
 void Display::ErrorPrint(const std::string &str, const std::string &name/*=""*/){
-	std::cout << DkRed << "[ERROR]";
-	if(!name.empty())
-		std::cout << " " << name << ":";
-	std::cout << " " << str << Reset << std::endl;
+	std::cout << ErrorStr(name) << str << Reset << std::endl;
 }
 
 void Display::InfoPrint(const std::string &str, const std::string &name/*=""*/){
-	std::cout << BtBlue << "[INFO]";
-	if(!name.empty())
-		std::cout << " " << name << ":";
-	std::cout << " " << str << Reset << std::endl;
+	std::cout << InfoStr(name) << str << Reset << std::endl;
 }
 
 void Display::OkayPrint(const std::string &str, const std::string &name/*=""*/){
-	std::cout << DkGreen << "[OK]";
-	if(!name.empty())
-		std::cout << " " << name << ":";
-	std::cout << " " << str << Reset << std::endl;
+	std::cout << OkayStr(name) << str << Reset << std::endl;
 }
 
 void Display::WarningPrint(const std::string &str, const std::string &name/*=""*/){
-	std::cout << DkYellow << "[WARNING]";
-	if(!name.empty())
-		std::cout << " " << name << ":";
-	std::cout << " " << str << Reset << std::endl;
+	std::cout << WarningStr(name) << str << Reset << std::endl;
 }
 
 bool Display::StatusPrint(bool errorStatus){
 	if (errorStatus) 
-		std::cout << Display::ErrorStr() << std::endl;
+		std::cout << ErrorStr() << std::endl;
 	else
-		std::cout << Display::OkayStr() << std::endl;
+		std::cout << OkayStr() << std::endl;
 	return errorStatus;
 }
